e19a3bbc15bdc88fac89341aaa15dec2bdff1ea6
Ahmed Hassan <64609713+AhmedDevOpsGenius@users.noreply.github.com>
2024-01-03 20:37:19 +0500

    Update outputs.tf

--- module/blob-storage/outputs.tf
+++ module/blob-storage/outputs.tf
@@ -1,11 +1,3 @@
 output "storage_account_id" {
   value = azurerm_storage_account.example.id
 }
-
-output "container_id" {
-  value = azurerm_storage_container.example.id
-}
-
-output "blob_id" {
-  value = azurerm_storage_blob.example.id
-}
