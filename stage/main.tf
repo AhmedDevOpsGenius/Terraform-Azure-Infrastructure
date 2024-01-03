c50e56ad30e4fea47ce9eedaa3a1f00244ef70dc
Ahmed Hassan <64609713+AhmedDevOpsGenius@users.noreply.github.com>
2024-01-03 20:42:00 +0500

    Update main.tf

--- stage/main.tf
+++ stage/main.tf
@@ -4,3 +4,10 @@ module "network" {
   location           = "West US 2"
   resource_group_name = "devops-amir-stage-rg"
 }
+
+module "blob_storage" {
+  source             = "../module/blob-storage"
+  resource_group_name = "devops-amir-stage-rg"
+  location           = "West US 2"
+  name               = "devops-amir-stage"
+}
