40b33a852200c74fe50c702714240a44afe7b3d4
Ahmed Hassan <64609713+AhmedDevOpsGenius@users.noreply.github.com>
2024-01-03 20:38:17 +0500

    Update main.tf

--- dev/main.tf
+++ dev/main.tf
@@ -4,3 +4,17 @@ module "network" {
   location           = "East US 2"
   resource_group_name = "devops-amir-dev-rg"
 }
+
+module "blob_storage" {
+  source                = "../module/blob-storage"
+  resource_group_name  = "devops-amir-dev-rg"
+  storage_account_name  = "devopsamirdevstore"
+  container_name        = "content"
+  blob_name             = "my-awesome-content.zip"
+  local_file_path       = "some-local-file.zip"
+}
+
+# Reference outputs from blob-storage module
+output "storage_account_id" {
+  value = module.blob_storage.storage_account_id
+}
