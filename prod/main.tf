adfc48b81692c11b56308ce0ab56afeec23a722a
Ahmed Hassan <64609713+AhmedDevOpsGenius@users.noreply.github.com>
2024-01-03 20:42:48 +0500

    Update main.tf

--- prod/main.tf
+++ prod/main.tf
@@ -1,6 +1,13 @@
 module "network" {
   source             = "../module"
   name               = "devops-amir-prod"
-  location           = "Central US"
+  location           = "West Europe"
   resource_group_name = "devops-amir-prod-rg"
+}
+
+module "blob_storage" {
+  source             = "../module/blob-storage"
+  resource_group_name = "devops-amir-prod-rg"
+  location           = "West Europe"
+  name               = "devops-amir-prod"
 }
