f363fdcdec0db2c03c571023dd5766f4908ff3c6
Ahmed Hassan <64609713+AhmedDevOpsGenius@users.noreply.github.com>
2024-01-05 03:35:37 +0500

    Update variable.tf

--- module/aks/variable.tf
+++ module/aks/variable.tf
@@ -1,41 +1,21 @@
-variable "name" {
+variable "client_id" {
   type        = string
-  description = "Name for the AKS cluster"
+  description = "Service Principal Client ID for AKS"
+}
+
+variable "client_secret" {
+  type        = string
+  description = "Service Principal Client Secret for AKS"
 }
 
 variable "location" {
   type        = string
   description = "Location for the AKS cluster"
+  default     = "East US 2"
 }
 
 variable "resource_group_name" {
   type        = string
-  description = "Name of the resource group"
-}
-
-variable "dns_prefix" {
-  type        = string
-  description = "DNS prefix for the AKS cluster"
-}
-
-variable "node_count" {
-  type        = number
-  description = "Number of nodes in the default node pool"
-  default     = 3
-}
-
-variable "vm_size" {
-  type        = string
-  description = "Size of the VMs in the default node pool"
-  default     = "Standard_DS2_v2"
-}
-
-variable "client_id" {
-  type        = string
-  description = "Service Principal Client ID"
-}
-
-variable "client_secret" {
-  type        = string
-  description = "Service Principal Client Secret"
+  description = "Name of the resource group for AKS"
+  default     = "devops-amir-dev-rg"
 }
