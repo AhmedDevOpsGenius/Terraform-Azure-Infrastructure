4004725ff8638da9abc5c4f72703ee9ce1ec5d8e
Ahmed Hassan <64609713+AhmedDevOpsGenius@users.noreply.github.com>
2024-01-05 03:39:25 +0500

    Update main.tf

--- prod/main.tf
+++ prod/main.tf
@@ -11,3 +11,10 @@ module "blob_storage" {
   location           = "West Europe"
   name               = "devops-amir-prod"
 }
+module "aks_cluster" {
+  source              = "../module/aks"
+  client_id           = "your-service-principal-client-id"
+  client_secret       = "your-service-principal-client-secret"
+  location            = "North Europe" 
+  resource_group_name = "devops-amir-prod-rg"
+}
