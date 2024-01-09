1af66adbe5e924b2db823e3d102748fb758526b0
Ahmed Hassan <64609713+AhmedDevOpsGenius@users.noreply.github.com>
2024-01-09 17:19:51 +0500

    Update variable.tf

--- module/blob-storage/variable.tf
+++ module/blob-storage/variable.tf
@@ -1,3 +1,14 @@
+variable "name" {
+  type        = string
+  description = "Name for this infrastructure"
+}
+
+variable "location" {
+  type        = string
+  description = "Name of the region for this infrastructure"
+  default     = "East US 2"
+}
+
 variable "resource_group_name" {
   type        = string
   description = "Name of the resource group"
@@ -6,19 +17,23 @@ variable "resource_group_name" {
 variable "storage_account_name" {
   type        = string
   description = "Name of the storage account"
+  default     = "uniqstoragenameamir"
 }
 
 variable "container_name" {
   type        = string
   description = "Name of the blob storage container"
+  default     = "uniquecontainernameamir"
 }
 
 variable "blob_name" {
   type        = string
   description = "Name of the blob"
+  default     = "blobamir"
 }
 
 variable "local_file_path" {
   type        = string
   description = "Local path of the file to upload to the blob"
+  default     = "/home/centos/file.txt"
 }
