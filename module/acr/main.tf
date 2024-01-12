536326777697e6eefc632182ddf105cd73600333
Ahmed Hassan <64609713+AhmedDevOpsGenius@users.noreply.github.com>
2024-01-12 15:34:15 +0500

    Update main.tf

--- module/acr/main.tf
+++ module/acr/main.tf
@@ -1,29 +1,14 @@
-# module/acr/main.tf
-
-variable "resource_group_name" {
-  type        = string
-  description = "Name of the resource group"
-}
-
-variable "location" {
-  type        = string
-  description = "Name of the region for this infrastructure"
-  default     = "East US 2"
-}
-
-variable "acr_name" {
-  type        = string
-  description = "Name of the Azure Container Registry"
-}
-
 resource "azurerm_container_registry" "example" {
   name                = var.acr_name
   resource_group_name = var.resource_group_name
   location            = var.location
-  sku                 = "Standard"
-  admin_enabled       = false
+  sku                 = "Basic"
 
-  network_rule {
-    ip_range = "0.0.0.0/0"
-  }
+  admin_enabled = false
+
+  // Additional configuration for the ACR resource
+}
+
+output "acr_id" {
+  value = azurerm_container_registry.example.id
 }
